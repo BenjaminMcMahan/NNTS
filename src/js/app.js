dc21bc2e5435c90138272dfda4d748b4e2363998
Benjamin McMahan <benjamin@linkedresponder.com>
2022-09-09 21:03:30 -0400

    Added contact form and footer

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
 import 'bootstrap';
+const firstName = document.getElementById("first_name");
+const contactText = document.getElementById("contactName");
+firstName.addEventListener("blur", function() {
+    if (firstName.value !== '') {
+        contactText.textContent = `${firstName.value}!`
+    } else {
+        contactText.textContent = '...?'
+    }
+});
